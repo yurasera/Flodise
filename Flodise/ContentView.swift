cbb9ae0c068f3ff798faa9c198a0828f77ba1a80
yurasera <yuhaya.lissera@gmail.com>
2026-07-22 23:29:15 +0700

    Initial Commit

---
+++ Flodise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flodise
+//
+//  Created by Yuhaya Lissera on 22/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flodise/FlodiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlodiseApp.swift
+//  Flodise
+//
+//  Created by Yuhaya Lissera on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct FlodiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
